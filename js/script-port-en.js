24c5cdcde2689e8bf633f79680b3b620ee6b75f8
André Luiz Alves <39349276+Oevidente@users.noreply.github.com>
2022-03-14 11:04:37 -0300

    Cria página do portifólio em inglês

---
+++ js/script-port-en.js
@@ -0,0 +1,250 @@
+const principal = document.querySelector('.principal');
+
+const menuPort = principal.querySelector('.menu-port');
+
+const tudo = menuPort.querySelector('.tudo');
+const idVisual = menuPort.querySelector('.id-visual');
+const midSociais = menuPort.querySelector('.mid-sociais');
+
+const editorial = menuPort.querySelector('.editorial');
+
+
+const trabalhos = principal.querySelector('.trabalhos');
+
+tudo.addEventListener('click', () => {
+
+  trabalhos.innerHTML = `<figure class="item-trabalho item01">
+  <img src="../img/galeria-trabalhos/tudo/Capas Hi - Aplicação.png" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item02">
+  <img src="../img/galeria-trabalhos/tudo/Capas Hi - Aplicação Carnaval.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item03">
+  <img src="../img/galeria-trabalhos/tudo/Capas Hi - Aplicação Coelhinhos.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item04">
+  <img src="../img/galeria-trabalhos/tudo/Capas Hi - Aplicação Dia das mulheres.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item05">
+  <img src="../img/galeria-trabalhos/tudo/Páginas Hi Aplicação.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item06">
+  <img src="../img/galeria-trabalhos/tudo/Páginas Hi aplicação 2.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item07">
+  <img src="../img/galeria-trabalhos/tudo/Páginas Hi aplicação 3.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item08">
+  <img src="../img/galeria-trabalhos/tudo/Embalagem de lanche.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item09">
+  <img src="../img/galeria-trabalhos/tudo/Dia mundial do burguer - capa.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item10">
+  <img src="../img/galeria-trabalhos/tudo/Post 5.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item11">
+  <img src="../img/galeria-trabalhos/tudo/Barbo.png" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item12">
+  <img src="../img/galeria-trabalhos/tudo/oooooooo.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item13">
+  <img src="../img/galeria-trabalhos/tudo/ooooos.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item14">
+  <img src="../img/galeria-trabalhos/tudo/Peça o seu.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item15">
+  <img src="../img/galeria-trabalhos/tudo/Sexta Santa.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item16">
+  <img src="../img/galeria-trabalhos/tudo/6.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item17">
+  <img src="../img/galeria-trabalhos/tudo/5.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item18">
+  <img src="../img/galeria-trabalhos/tudo/FUNDO.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item19">
+  <img src="../img/galeria-trabalhos/tudo/Logo horizontal branco.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item20">
+  <img src="../img/galeria-trabalhos/tudo/Logo horizontal preto.png" alt="" class="img-trabalho">
+</figure>`;
+
+})
+
+idVisual.addEventListener('click', () => {
+  trabalhos.innerHTML = `<figure class="item-trabalho item01">
+  <img src="../img/galeria-trabalhos/id-visual/Isoladas 2 (3).png" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item02">
+  <img src="../img/galeria-trabalhos/id-visual/Isoladas 2 (4).png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item03">
+  <img src="../img/galeria-trabalhos/id-visual/Isoladas 2 (5).png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item04">
+  <img src="../img/galeria-trabalhos/id-visual/Isoladas 2.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item05">
+  <img src="../img/galeria-trabalhos/id-visual/1.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item06">
+  <img src="../img/galeria-trabalhos/id-visual/2.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item07">
+  <img src="../img/galeria-trabalhos/id-visual/3.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item08">
+  <img src="../img/galeria-trabalhos/id-visual/4.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item09">
+  <img src="../img/galeria-trabalhos/id-visual/5.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item10">
+  <img src="../img/galeria-trabalhos/id-visual/6.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item11">
+  <img src="../img/galeria-trabalhos/id-visual/7.png" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item12">
+  <img src="../img/galeria-trabalhos/id-visual/8.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item13">
+  <img src="../img/galeria-trabalhos/id-visual/9.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item14">
+  <img src="../img/galeria-trabalhos/id-visual/10.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item15">
+  <img src="../img/galeria-trabalhos/id-visual/11.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item16">
+  <img src="../img/galeria-trabalhos/id-visual/12.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item17">
+  <img src="../img/galeria-trabalhos/id-visual/Logo Humanas Verde.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item18">
+  <img src="../img/galeria-trabalhos/id-visual/Branco.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item19">
+  <img src="../img/galeria-trabalhos/id-visual/Dourado.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item20">
+  <img src="../img/galeria-trabalhos/id-visual/Prata.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item21">
+  <img src="../img/galeria-trabalhos/id-visual/Prancheta 1 cópia 3.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item22">
+  <img src="../img/galeria-trabalhos/id-visual/Prancheta 1 cópia 8.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item23">
+  <img src="../img/galeria-trabalhos/id-visual/Prancheta 1 cópia 4.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item24">
+  <img src="../img/galeria-trabalhos/id-visual/Prancheta 1 cópia 7.png" alt="" class="img-trabalho">
+</figure>`;
+})
+
+midSociais.addEventListener('click', () => {
+  trabalhos.innerHTML = `<figure class="item-trabalho item01">
+  <img src="../img/galeria-trabalhos/social-midia/Dia mundial do burguer - capa.png" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item02">
+  <img src="../img/galeria-trabalhos/social-midia/Tela 1.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item03">
+  <img src="../img/galeria-trabalhos/social-midia/Tela 2.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item04">
+  <img src="../img/galeria-trabalhos/social-midia/Tela 3.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item05">
+  <img src="../img/galeria-trabalhos/social-midia/Tela final.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item06">
+  <img src="../img/galeria-trabalhos/social-midia/Peça o seu.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item07">
+  <img src="../img/galeria-trabalhos/social-midia/Combo Simples.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item08">
+  <img src="../img/galeria-trabalhos/social-midia/Combo Duplo.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item09">
+  <img src="../img/galeria-trabalhos/social-midia/Combo Nobre.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item10">
+  <img src="../img/galeria-trabalhos/social-midia/Post 5.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item11">
+  <img src="../img/galeria-trabalhos/social-midia/Barbo.png" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item12">
+  <img src="../img/galeria-trabalhos/social-midia/oooooooo.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item13">
+  <img src="../img/galeria-trabalhos/social-midia/ooooos.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item14">
+  <img src="../img/galeria-trabalhos/social-midia/Sexta Santa.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item15">
+  <img src="../img/galeria-trabalhos/social-midia/Dia do vestibulando.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item16">
+  <img src="../img/galeria-trabalhos/social-midia/Matrícula 2.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item17">
+  <img src="../img/galeria-trabalhos/social-midia/Matrícula 3.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item18">
+  <img src="../img/galeria-trabalhos/social-midia/POST SSA.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item19">
+  <img src="../img/galeria-trabalhos/social-midia/Prancheta 2.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item20">
+  <img src="../img/galeria-trabalhos/social-midia/Prancheta 3.png" alt="" class="img-trabalho">
+</figure>`;
+})
+
+
+editorial.addEventListener('click', () => {
+  trabalhos.innerHTML = `<figure class="item-trabalho item01">
+  <img src="../img/galeria-trabalhos/editorial/Capas Hi - Aplicação Carnaval.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item02">
+  <img src="../img/galeria-trabalhos/editorial/Capas Hi - Aplicação Coelhinhos.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item03">
+  <img src="../img/galeria-trabalhos/editorial/Capas Hi - Aplicação Dia das mulheres.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item04">
+  <img src="../img/galeria-trabalhos/editorial/Capas Hi - Aplicação.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item05">
+  <img src="../img/galeria-trabalhos/editorial/Páginas Hi aplicação -1.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item06">
+  <img src="../img/galeria-trabalhos/editorial/Páginas Hi aplicação -2.png" alt="" class="img-trabalho">
+
+</figure>
+<figure class="item-trabalho item07">
+  <img src="../img/galeria-trabalhos/editorial/Páginas Hi aplicação 2.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item08">
+  <img src="../img/galeria-trabalhos/editorial/Páginas Hi aplicação 3.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item09">
+  <img src="../img/galeria-trabalhos/editorial/Páginas Hi Aplicação-1.png" alt="" class="img-trabalho">
+</figure>
+<figure class="item-trabalho item10">
+  <img src="../img/galeria-trabalhos/editorial/Páginas Hi Aplicação.png" alt="" class="img-trabalho">
+</figure>`;
+})
